e81118b2582ec289ce1f23eb21e441de901d6b16
amusukwa <amusukwa@gmail.com>
2024-03-27 05:42:45 -0700

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@ This module defines a Cache class for storing data in Redis.
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 class Cache:
     """
@@ -20,10 +20,10 @@ class Cache:
         """
         Initializes the Cache class.
 
-        Initializes an instance of the Redis client and flushes the Redis database.
+        Initializes instance of Redis client and flushes the Redis database.
         """
         self._redis = redis.Redis()
-        self._redis.flushdb()  # Flush the Redis database on initialization
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
@@ -38,3 +38,45 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+        """
+        data from Redis using given key, applying optional conversion function.
+
+        Args:
+            key: The key to retrieve data from Redis.
+            fn: An optional conversion function to apply to the retrieved data.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieves a string value from Redis using the given key.
+
+        Args:
+            key: The key to retrieve the string value from Redis.
+
+        Returns:
+            Optional[str]: The retrieved string value, or None if the key does not exist.
+        """
+        return self.get(key, str.decode)
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieves an integer value from Redis using the given key.
+
+        Args:
+            key: The key to retrieve the integer value from Redis.
+
+        Returns:
+            Optional[int]: The retrieved integer value, or None if the key does not exist or the value is not an integer.
+        """
+        return self.get(key, int)
